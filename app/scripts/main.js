57258ae38c095f8f92855bdd9993f145a58ccc58
MikeESL <mike.reser.03@gmail.com>
2014-05-05 15:09:54 -0400

    wrote tests for constructors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,20 @@
-
 $(document).ready(function() {
-	
+
 	$(".FighterOne").click(function(e) {
 		e.preventDefault();
 		clint.weapon.fire(harry);
 		//console.log ("harry has " + harry.life + " points of life left");
 		$(".FighterOneResult").append(" Harry has " + harry.life + " points of life left. " );
-		
+
 });		
 	$(".FighterTwo").click(function(e) {
 		e.preventDefault();
 		harry.weapon.fire(clint);
 		//console.log ("clint has " + clint.life + " points of life left");
 		$(".FighterTwoResult").append(" Clint has " + clint.life + " points of life left. " );
-		
+
 	});
-	
+
 
 });
 
@@ -34,17 +33,17 @@ function FighterOne (name, weapon){
 				if (this.life <= 0){
 				//alert (this.name + " died. Game Over");
 				$(".FighterTwoResultImg").append("<img src=\"images/dirtyharry.png\">");
-				
+
 				}
-				
+
 };
 }
 
-// FighterOne.prototype.addDamage = function($el, weapon) {
-// 	  var damagePath = $(".FighterTwoResultImg").val();
-// 	  var endDamage = damagePath + "<img src=\"images/dirtyharry.png\">";
-// 		$el.append(endDamage);
-// 	};
+FighterOne.prototype.addDamage = function($el, weapon) {
+		  var damagePath = $(".FighterTwoResultImg").val();
+		  var endDamage = damagePath + "<img src=\"images/dirtyharry.png\">";
+			$el.append(endDamage);
+		};
 
 
 function FighterTwo (name, weapon){
@@ -56,9 +55,9 @@ function FighterTwo (name, weapon){
 				if (this.life <= 0){
 				//alert (this.name + " died. Game Over");
 				$(".FighterOneResultImg").append("<img src=\"images/dirtyharry.png\">");
-				
+
 		}
-		
+
 };
 }
 
@@ -85,81 +84,48 @@ var clint = new FighterOne("clint", fist);
 var harry = new FighterTwo("harry", gun);
 var mike = new FighterOne ("mike");
 
-// another try at this:
-// function FighterOne (name){
-// 		this.name = name;
-// 		this.life = 100;
-// 		this.addDamage = function (weapon){
-// 				if (this.life <= 0){
-// 					alert (this.name + "died. Game Over");
-// 				}
-// 				this.life = this.life - weapon.damage;
-// };
-// }
-
-// function FighterTwo (name){
-// 		this.name = name;
-// 		this.life = 100;
-// 		this.addDamage = function(weapon){
-// 				if (this.life <= 0){
-// 		}
-// 		this.life = this.life - weapon.damage;
-// };
-// }
-// function Weapon (type, damage){
-// 		this.type = type || "fist";
-// 		this.damage = damage || 5;
-// 		this.fire = function (target){
-// 			target.addDamage(this);
-
-// 		}
-// };
-// var crowbar = new Weapon ("crowbar", 20);
-// var knife = new Weapon ("knife", 40);
-// var gun = new Weapon ("gun", 60);
-// var bazooka = new Weapon ("bazzoka", 100);
-// var clint = new FighterOne("clint");
-// var harry = new FighterTwo("harry");
-
-//});
-
-//attempt to get 3 constructors intetacting. Abandoned for now
-// function Monster (name, weapon) {
-// 		this.name = name;
-// 		this.isMonster = true;
-// 		this.isHuman = false;
-// 		this.isAlive = true;
-// 		this.bite = function(Vampire) {
-// 					Vampire.isAlive = false;
-
-// 		};
-// }
-
-// function Vampire (name) {
-// 		this.name = name;
-// 		this.isVampire = true;
-// 		this.isHuman = false;
-// 		this.isAlive = true;
-// 		this.bite = function(Monster){
-// 					Monster.isAlive = false; 
-
-// };
-// }
-// ,
-// function Wizard (name, spell) {
-// 	this.name = name;
-// 	this.isWizard = true;
-// 	this.spellOne = function(Monster){
-// 			Monster.isHuman = true;
-// };
-// 	this.spellTwo = function(Vampire)
-// 			Monster.isHuman = true;}
-// }
 
 
-// var franky = new Monster("Franky");
-// var dracula = new Vampire("Dracula");
-// var merlin = new Wizard ("Merlin");
+
+//---------------------------------------------------------
+
+// //attempt to get 3 constructors intetacting. Abandoned for now
+// // function Monster (name, weapon) {
+// // 		this.name = name;
+// // 		this.isMonster = true;
+// // 		this.isHuman = false;
+// // 		this.isAlive = true;
+// // 		this.bite = function(Vampire) {
+// // 					Vampire.isAlive = false;
+
+// // 		};
+// // }
+
+// // function Vampire (name) {
+// // 		this.name = name;
+// // 		this.isVampire = true;
+// // 		this.isHuman = false;
+// // 		this.isAlive = true;
+// // 		this.bite = function(Monster){
+// // 					Monster.isAlive = false; 
+
+// // };
+// // }
+// // ,
+// // function Wizard (name, spell) {
+// // 	this.name = name;
+// // 	this.isWizard = true;
+// // 	this.spellOne = function(Monster){
+// // 			Monster.isHuman = true;
+// // };
+// // 	this.spellTwo = function(Vampire)
+// // 			Monster.isHuman = true;}
+// // }
+
+
+// // var franky = new Monster("Franky");
+// // var dracula = new Vampire("Dracula");
+// // var merlin = new Wizard ("Merlin");
 
 
 
@@ -168,18 +134,18 @@ var mike = new FighterOne ("mike");
 
 
 
-// // $("form").submit(function(event) {
-// //   		if ($("input:first" ).val() === "jquery") {
-// //     $(".display").text("Validated!").show();
-// //     alert("Thank you for typing correctly");
-// //     return;
-// //   }
+// // // $("form").submit(function(event) {
+// // //   		if ($("input:first" ).val() === "jquery") {
+// // //     $(".display").text("Validated!").show();
+// // //     alert("Thank you for typing correctly");
+// // //     return;
+// // //   }
  
   
-// //   $(".display").text("Incorrect, genius!").show().fadeOut(5000);
-// //   alert("No Way!");
-// //   event.preventDefault();
-// // });
+// // //   $(".display").text("Incorrect, genius!").show().fadeOut(5000);
+// // //   alert("No Way!");
+// // //   event.preventDefault();
+// // // });
 
 
 
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,9 +3,26 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('Constructor Testing', function () {
+        describe('FighterOne Constructor Test', function () {
+        	 var testFighterOne = new FighterOne ();
+			 var testFighterOneKnown = new FighterOne ("Louis", "hammer")
+			 it('should be an instance of FighterOne', function () {
+			 		expect(testFighterOne).to.be.an.instanceof(FighterOne);
+        describe('FighterTwo Constructor Test', function (){
+            var testFighterTwo = new FighterTwo ();
+            var testFighterTwoKnown = new FighterTwo ("Jim", "sword")
+            it ('shoud be an instance of FighterOne', function (){
+                    expect(testFighterTwo).to.be.an.instanceof(FighterTwo);
+        describe ('Weapon Constructor Testing', function (){
+            var testWeapon = new Weapon ();
+            var testWeaponKnown = new Weapon ("sword", 30)
+            it ('should be an instance of Weapon', function (){
+                    expect(testWeapon).to.be.an.instanceof(Weapon);
+            })
+        })
+            })
+        })
 
             });
         });
